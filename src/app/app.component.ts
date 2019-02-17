16739637d66d0c4b4589c46660ccbcaef5297c42
gianni <gianniresende@gmail.com>
2019-02-17 15:33:02 -0300

    removing manual login from AppComponent.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,13 @@ import { NSAngular2TokenService } from "./shared/ns-angular2-token/ns-angular2-t
 export class AppComponent {
   public constructor(private tokenService: NSAngular2TokenService){
     this.tokenService.init({
-      apiBase: "http://10.0.3.3:3000",
+        apiBase: "http://10.0.3.3:3000",
         globalOptions:{
           headers: {
             "Content-Type": "application/json",
             "Accept": "application/vnd.taskmanager.v2"
           }
-      }
-    })
-
-    this.tokenService.signIn({
-      email: "estoutestando@gmail.com",
-      password: "123456789"
-    }).subscribe(res => console.dir(res))
+        }
+      })
+    }
   }
- }

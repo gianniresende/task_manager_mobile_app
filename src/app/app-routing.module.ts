1922264ac6501d8117e53f8ae4efd53cd1dc56af
gianni <gianniresende@gmail.com>
2019-02-17 15:07:41 -0300

    fixing indentation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,14 +8,14 @@ import { SignUpComponent } from "./sign-up/sign-up.component";
 
 
 const routes: Routes = [
-    { path: "", redirectTo: "/sign-in", pathMatch: "full" },
-    { path: "sign-in", component: SignInComponent },
-    { path: "sign-up", component: SignUpComponent }   
+  { path: "", redirectTo: "/sign-in", pathMatch: "full" },
+  { path: "sign-in", component: SignInComponent },
+  { path: "sign-up", component: SignUpComponent }   
 
 ];
 
 @NgModule({
-    imports: [NativeScriptRouterModule.forRoot(routes)],
-    exports: [NativeScriptRouterModule]
+  imports: [NativeScriptRouterModule.forRoot(routes)],
+  exports: [NativeScriptRouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,20 @@ import { NSAngular2TokenService } from "./shared/ns-angular2-token/ns-angular2-t
     templateUrl: "./app.component.html"
 })
 export class AppComponent {
-    public constructor(private tokenService: NSAngular2TokenService){
-        this.tokenService.init({
-            apiBase: "http://10.0.3.3:3000",
-            globalOptions:{
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/vnd.taskmanager.v2"
-                }
-            }
-        })
+  public constructor(private tokenService: NSAngular2TokenService){
+    this.tokenService.init({
+      apiBase: "http://10.0.3.3:3000",
+        globalOptions:{
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/vnd.taskmanager.v2"
+          }
+      }
+    })
 
-        this.tokenService.signIn({
-            email: "estoutestando@gmail.com",
-            password: "123456789"
-        }).subscribe(res => console.dir(res))
-    }
+    this.tokenService.signIn({
+      email: "estoutestando@gmail.com",
+      password: "123456789"
+    }).subscribe(res => console.dir(res))
+  }
  }
